5fe5cdb21e2c7d1aacf62842a4b6a42c7144877e
Itspacome <pacome.perrichot@gmail.com>
2018-04-11 23:22:45 +0200

    Bot twitter

--- 1_test.rb
+++ 1_test.rb
@@ -1,5 +1,23 @@
+# require 'pry'
+
 require 'dotenv'
 
 Dotenv.load
 
-puts ENV["TWITTER_API_SECRET"]
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+	config.consumer_key        = ENV["TWITTER_API_KEY"]
+  	config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  	config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  	config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+end
+
+# binding.pry
+
+journalist =  ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap"]
+
+journalist.map { |user|
+
+client.update(" Salut moi c'est Pacô, je suis élève à The Hacking Projet une formation gratuite au code et je viens de faire un bot Twitter trop cool ! " + user.to_s)
+}
